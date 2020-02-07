bbe5b97e78ff0add73cf1bd0821e9a0db19ba65e
Jhol H Winchester <small.anproject@gmail.com>
2020-02-07 14:37:25 -0300

    :hammer: finalizando parte web

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
-	const counter = 0;
+  const [devs, setDevs] = useState([]);
 
-	function incrementCounter() {
-		alert('Hello')
-	}
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-	return (
-		<>
-			<h1>Contator: 0</h1>
-			<button onClick={incrementCounter}>Incrementar</button>
-		</>
-	);
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
